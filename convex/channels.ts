75842ad262508c89a4b04089d14c39ea356cd045
sunce <oy.zf@qq.com>
2024-09-07 18:42:19 +0800

    后端服务: 添加创建channel API和hook

--- convex/channels.ts
+++ convex/channels.ts
@@ -1,5 +1,5 @@
 import {getAuthUserId} from "@convex-dev/auth/server";
-import {query} from "./_generated/server";
+import {mutation, query} from "./_generated/server";
 import {v} from "convex/values";
 
 // API: 获取workspace下channel
@@ -19,3 +19,26 @@ export const get = query({
         return channels;
     }
 })
+
+// API: 创建一个channel
+export const create = mutation({
+    args: {
+        workspaceId: v.id("workspaces"),
+        name: v.string()
+    },
+    handler: async (ctx, args) => {
+        const userId = await getAuthUserId(ctx)
+        if (!userId) throw new Error("未授权行为.");
+        const member = await ctx.db.query("members").withIndex("by_workspace_id_user_id",
+            (q) => q.eq("workspaceId", args.workspaceId).eq("userId", userId)).unique();
+        if (!member || "admin" !== member.role) throw new Error("未授权行为.");
+
+        const chId = await ctx.db.insert("channels", {
+            workspaceId: args.workspaceId,
+            name: args.name.replace(/\s+/g,"-")
+        })
+
+        return chId
+    }
+})
+
