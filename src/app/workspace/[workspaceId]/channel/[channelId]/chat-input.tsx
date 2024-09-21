2574c3a0f92cf8fbfa895d0a1cccf6d7ae0b1a36
sunce <oy.zf@qq.com>
2024-09-21 23:21:56 +0800

    频道页面-聊天输入组件: 修复图片上传bug

--- src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
@@ -5,7 +5,6 @@ import {useCreateMessage} from "@/features/messages/api/use-create-message";
 import {useWorkspaceId} from "@/hooks/use-workspace-id";
 import {useChannelId} from "@/hooks/use-channel-id";
 import {useGenerateUploadUrl} from "@/features/upload/api/use-generate-upload-url";
-import {Id} from "../../../../../../convex/_generated/dataModel";
 import {toast} from "sonner";
 
 // 动态载入以支持热重载
@@ -14,7 +13,6 @@ const Editor = dynamic(() => import("@/components/editor"), {ssr: false})
 export function ChatInput({placeholder}: { placeholder: string }) {
     const [editorKey, setEditorKey] = useState(0)
     const editorRef = useRef<Quill | null>(null)
-    const [storageId, setStorageId] = useState<Id<"_storage"> | null>(null)
     const {mutate: submitMessage, isPending: submittingMessage} = useCreateMessage()
     const {mutate: generateUploadUrl} = useGenerateUploadUrl()
     const workspaceId = useWorkspaceId()
@@ -31,38 +29,50 @@ export function ChatInput({placeholder}: { placeholder: string }) {
             if (image) {
                 // done: 上传消息图片
                 // Step 1: Get a short-lived upload URL
-                await generateUploadUrl({},
-                    {
-                        onSuccess: async (postUrl) => {
-                            if (!postUrl) throw new Error("Url找不到");
-                            // Step 2: POST the file to the URL
-                            const result = await fetch(postUrl!, {
-                                method: "POST",
-                                headers: {"Content-Type": image!.type},
-                                body: image,
-                            });
-                            if (!result.ok) throw new Error("图片上传失败");
-                            const {storageId} = await result.json();
-                            setStorageId(storageId)
-                            console.log("图片上传成功, id: ", storageId);
-                        }
-                    });
+                await generateUploadUrl({}, {
+                    onSuccess: async (postUrl) => {
+                        if (!postUrl) throw new Error("Url找不到");
+                        // Step 2: POST the file to the URL
+                        const result = await fetch(postUrl!, {
+                            method: "POST",
+                            headers: {"Content-Type": image!.type},
+                            body: image,
+                        });
+                        if (!result.ok) throw new Error("图片上传失败");
+                        const {storageId} = await result.json();
+                        console.log("图片上传成功, id: ", storageId);
+                        await submitMessage({
+                            workspaceId,
+                            channelId,
+                            body,
+                            image: storageId
+                        }, {
+                            onSuccess: () => {
+                                console.log("message submitted")
+                            },
+                            onError: () => {
+                                console.error("message submitted invalid")
+                            },
+                        })
+                    }
+                });
+            } else {
+                await submitMessage({workspaceId, channelId, body}, {
+                    onSuccess: () => {
+                        console.log("message submitted")
+                    },
+                    onError: () => {
+                        console.error("message submitted invalid")
+                    },
+                })
             }
-            await submitMessage({workspaceId, channelId, body, image: storageId ? storageId : undefined}, {
-                onSuccess: () => {
-                    console.log("message submitted")
-                },
-                onError: () => {
-                    console.error("message submitted invalid")
-                },
-            })
+
             toast.success("消息发送成功.")
         } catch (e) {
             toast.error("消息发送  失败.")
         } finally {
             setEditorKey((k) => k + 1)
             editorRef.current?.enable(true)
-            setStorageId(null)
         }
     }
 
