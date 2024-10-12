55eb564ed372d436d2c1efc872b20f05d07570f8
sunce <oy.zf@qq.com>
2024-10-12 19:24:06 +0800

    后端服务-fix: 修改id获取member API hook的参数类型, 类型约束在hook内部

--- src/features/members/api/use-get-member.ts
+++ src/features/members/api/use-get-member.ts
@@ -3,12 +3,12 @@ import {api} from "../../../../convex/_generated/api"
 import {Id} from "../../../../convex/_generated/dataModel"
 
 interface useGetMemberProps {
-    memberId: Id<"members">,
+    memberId: string,
 }
 
 // 获取member API hook
 export const useGetMember = ({memberId}: useGetMemberProps) => {
-    const data = useQuery(api.members.getById, {memberId})
+    const data = useQuery(api.members.getById, {memberId: memberId as Id<"members">})
     const isLoading = data === undefined
 
     return {
