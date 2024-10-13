de623c12b40a8420fa742b8b4ef5a78ff6492f7d
sunce <oy.zf@qq.com>
2024-10-13 11:22:39 +0800

    个人信息面板页面-add: 添加对成员删除修改的二次确认

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -11,6 +11,7 @@ import {useWorkspaceId} from "@/hooks/use-workspace-id";
 import {toast} from "sonner";
 import {useRouter} from "next/navigation";
 import {DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger} from "@/components/ui/dropdown-menu";
+import useApproval from "@/hooks/use-confirm";
 
 interface ProfileProps {
     memberId: string;
@@ -25,7 +26,12 @@ export default function Profile({memberId, onClose}: ProfileProps) {
     const {mutate: removeMember, isPending: removingMember} = useRemoveMember()
     const {data: member, isLoading: loadingMember} = useGetMember({memberId})
     const router = useRouter()
+    const [LeaveDialog, comfirmLeave] = useApproval("离开工作空间", "你确定要离开么?")
+    const [RemoveDialog, comfirmRemove] = useApproval("删除成员", `你确定要删除${member?.user.name}么?`)
+    const [UpdateDialog, comfirmUpdate] = useApproval("修改成员身份", `你确定要修改${member?.user.name}的角色么?`)
     const onRemoveMember = async () => {
+        const ok = await comfirmRemove()
+        if (!ok) return;
         await removeMember({id: memberId}, {
             onSuccess: data => {
                 toast.success("操作成功")
@@ -36,7 +42,22 @@ export default function Profile({memberId, onClose}: ProfileProps) {
         });
         router.replace(`${window.location.origin}/workspace/${workspaceId}`)
     }
+    const onLeaveMember = async () => {
+        const ok = await comfirmLeave()
+        if (!ok) return;
+        await removeMember({id: memberId}, {
+            onSuccess: data => {
+                toast.success("操作成功")
+            },
+            onError: error => {
+                toast.error("操作失败:" + error.message)
+            },
+        });
+        router.replace(`${window.location.origin}`)
+    }
     const onUpdateMember = async (role: "admin" | "member") => {
+        const ok = await comfirmUpdate()
+        if (!ok) return;
         await updateMember({id: memberId, role}, {
             onSuccess: data => {
                 toast.success("操作成功")
@@ -54,83 +75,89 @@ export default function Profile({memberId, onClose}: ProfileProps) {
         )
     }
     return (
-        <div className={"thread-panel h-full w-full flex-col flex bg-[#fdfcfa]"}>
-            <div className={"thread-header h-[49px] flex justify-between items-center p-4 border-b"}>
-                <p className={"text-lg font-bold"}>Profile</p>
-                <Button variant={"ghost"} className={""} size={"icon_sm"} onClick={onClose}>
-                    <XIcon className={"size-5 stroke-2"}/>
-                </Button>
-            </div>
-            {!member && (
-                <div className={"flex h-full gap-x-2 items-center justify-center"}>
-                    <AlertTriangle className={" size-7 text-muted-foreground"}/>
-                    <p className={"text-muted-foreground text-sm"}>成员信息未找到</p>
+        <>
+            <LeaveDialog/>
+            <RemoveDialog/>
+            <UpdateDialog/>
+            <div className={"thread-panel h-full w-full flex-col flex bg-[#fdfcfa]"}>
+                <div className={"thread-header h-[49px] flex justify-between items-center p-4 border-b"}>
+                    <p className={"text-lg font-bold"}>Profile</p>
+                    <Button variant={"ghost"} className={""} size={"icon_sm"} onClick={onClose}>
+                        <XIcon className={"size-5 stroke-2"}/>
+                    </Button>
                 </div>
-            )}
-            {member && (
-                <>
-                    <div className={"flex flex-col p-4 items-center justify-center"}>
-                        <Avatar
-                            className="max-w-[128px] max-h-[128px] size-full hover:opacity-75 transition rounded-md">
-                            <AvatarImage src={member.user.image} className={"rounded-md"}/>
-                            <AvatarFallback className="aspect-square text-6xl rounded-md bg-sky-400 text-white">
-                                {member.user.name?.charAt(0).toUpperCase()}
-                            </AvatarFallback>
-                        </Avatar>
-                        <div className={"flex flex-col p-4 items-center"}>
-                            <p className={"font-bold text-xl"}>{member.user.name}</p>
-                            {currentMember?.role === "admin" && currentMember._id !== memberId && (
-                                <div className={"flex items-center mt-4 gap-2"}>
-                                    <DropdownMenu>
-                                        <DropdownMenuTrigger asChild>
-                                            <Button variant={"outline"} className={"w-full capitalize"}>
-                                                {member.role === "admin" ? "管理员" : "普通成员"} <ChevronDownIcon
-                                                className={"size-4 ml-2"}/>
-                                            </Button>
-                                        </DropdownMenuTrigger>
-                                        <DropdownMenuContent>
-                                            <DropdownMenuItem onClick={() => {
-                                                onUpdateMember(member.role === "admin" ? "member" : "admin")
-                                            }} className="cursor-pointer capitalize hover:bg-[#f0eeeb]">
-                                                <div className="flex items-center gap-1">
-                                                    {member.role === "admin" ? "普通成员" : "管理员"}
-                                                </div>
-                                            </DropdownMenuItem>
-                                        </DropdownMenuContent>
-                                    </DropdownMenu>
-                                    <Button variant={"outline"} className={"w-full capitalize"}
-                                            onClick={onRemoveMember}>
-                                        删除
-                                    </Button>
-                                </div>
-                            )}
-                            {currentMember?.role !== "admin" && currentMember?._id === memberId && (
-                                <div className={"flex items-center mt-4 gap-2"}>
-                                    <Button variant={"outline"}
-                                            className={"w-full capitalize hover:bg-red-600 hover:text-white"}
-                                            onClick={onRemoveMember}>
-                                        退出工作空间
-                                    </Button>
-                                </div>
-                            )}
-                        </div>
+                {!member && (
+                    <div className={"flex h-full gap-x-2 items-center justify-center"}>
+                        <AlertTriangle className={" size-7 text-muted-foreground"}/>
+                        <p className={"text-muted-foreground text-sm"}>成员信息未找到</p>
                     </div>
-                    <Separator/>
-                    <div className={"flex flex-col p-4"}>
-                        <p className={"font-bold text-md mb-4"}>联系方式</p>
-                        <div className={"flex items-center gap-2"}>
-                            <div className={"size-9 rounded-md bg-muted flex items-center justify-center"}>
-                                <MailIcon className={"size-4"}/>
-                            </div>
-                            <div className={"flex flex-col"}>
-                                <p className={"text-sm font-semibold"}>邮箱地址</p>
-                                <Link className={"text-sm hover:underline text-[#007bbb]"}
-                                      href={`mailto:${member.user.email}`}>{member.user.email}</Link>
+                )}
+                {member && (
+                    <>
+                        <div className={"flex flex-col p-4 items-center justify-center"}>
+                            <Avatar
+                                className="max-w-[128px] max-h-[128px] size-full hover:opacity-75 transition rounded-md">
+                                <AvatarImage src={member.user.image} className={"rounded-md"}/>
+                                <AvatarFallback className="aspect-square text-6xl rounded-md bg-sky-400 text-white">
+                                    {member.user.name?.charAt(0).toUpperCase()}
+                                </AvatarFallback>
+                            </Avatar>
+                            <div className={"flex flex-col p-4 items-center"}>
+                                <p className={"font-bold text-xl"}>{member.user.name}</p>
+                                {currentMember?.role === "admin" && currentMember._id !== memberId && (
+                                    <div className={"flex items-center mt-4 gap-2"}>
+                                        <DropdownMenu>
+                                            <DropdownMenuTrigger asChild>
+                                                <Button variant={"outline"} className={"w-full capitalize"}>
+                                                    {member.role === "admin" ? "管理员" : "普通成员"} <ChevronDownIcon
+                                                    className={"size-4 ml-2"}/>
+                                                </Button>
+                                            </DropdownMenuTrigger>
+                                            <DropdownMenuContent>
+                                                <DropdownMenuItem onClick={() => {
+                                                    onUpdateMember(member.role === "admin" ? "member" : "admin")
+                                                }} disabled={updatingMember}
+                                                                  className="cursor-pointer capitalize hover:bg-[#f0eeeb]">
+                                                    <div className="flex items-center gap-1">
+                                                        {member.role === "admin" ? "普通成员" : "管理员"}
+                                                    </div>
+                                                </DropdownMenuItem>
+                                            </DropdownMenuContent>
+                                        </DropdownMenu>
+                                        <Button variant={"outline"} className={"w-full capitalize"}
+                                                onClick={onRemoveMember} disabled={removingMember}>
+                                            删除
+                                        </Button>
+                                    </div>
+                                )}
+                                {currentMember?.role !== "admin" && currentMember?._id === memberId && (
+                                    <div className={"flex items-center mt-4 gap-2"}>
+                                        <Button variant={"outline"}
+                                                className={"w-full capitalize hover:bg-red-600 hover:text-white"}
+                                                onClick={onLeaveMember} disabled={removingMember}>
+                                            退出工作空间
+                                        </Button>
+                                    </div>
+                                )}
                             </div>
                         </div>
-                    </div>
-                </>
-            )}
-        </div>
+                        <Separator/>
+                        <div className={"flex flex-col p-4"}>
+                            <p className={"font-bold text-md mb-4"}>联系方式</p>
+                            <div className={"flex items-center gap-2"}>
+                                <div className={"size-9 rounded-md bg-muted flex items-center justify-center"}>
+                                    <MailIcon className={"size-4"}/>
+                                </div>
+                                <div className={"flex flex-col"}>
+                                    <p className={"text-sm font-semibold"}>邮箱地址</p>
+                                    <Link className={"text-sm hover:underline text-[#007bbb]"}
+                                          href={`mailto:${member.user.email}`}>{member.user.email}</Link>
+                                </div>
+                            </div>
+                        </div>
+                    </>
+                )}
+            </div>
+        </>
     )
 }
